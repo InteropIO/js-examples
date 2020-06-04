faf88c1ddd28d2e51e85b2ffe12227d85eaa1df8
Hristo Ivanov <47400757+arjunah@users.noreply.github.com>
2020-06-04 18:30:45 +0300

    Channels publisher reworked

--- channels/channels-publisher/src/index.js
+++ channels/channels-publisher/src/index.js
@@ -1,59 +1,98 @@
-Glue({
-  channels: true
-}).then(glue => {
-  // Convenient way of using glue throughout your application
-  window.glue = glue
+let channelNameItem;
+let channelColorItem;
+let currentChannelInput;
+let publishToCurrentBtn;
+let selectChannelAlert;
+let channelListItem;
+let selectedChannelInput;
+let publishToSelectedBtn;
 
-  const symbolListElement = document.getElementById('symbolList')
-  const infoFromSubscriberElement = document.getElementById(
-    'infoFromSubscriber'
-  )
-  const errorElement = document.getElementById('error')
+window.addEventListener("DOMContentLoaded", async () => {
+    channelNameItem = document.getElementById("channel-name");
+    channelColorItem = document.getElementById("channel-color");
+    currentChannelInput = document.getElementById("data-input-current");
+    publishToCurrentBtn = document.getElementById("publish-current-button");
+    selectChannelAlert = document.getElementById("select-channel-alert");
+    channelListItem = document.getElementById("channel-list");
+    selectedChannelInput = document.getElementById("data-input-selected");
+    publishToSelectedBtn = document.getElementById("publish-selected-button");
 
-  const symbols = ['Apple (APPL)', 'Google (GOOGL)', 'Microsoft (MSFT)']
+    // Initialize the Glue42 library.
+    await initializeGlue42()
+        .catch(error => { 
+            console.error(error); 
+            return 
+        });
 
-  symbols.forEach(symbol => {
-    const symbolElement = document.createElement('li')
-    symbolElement.textContent = symbol
-    symbolElement.onclick = () => {
-      // Each time we click on a symbol we will update the context of the current channel with publish method
-      // any subscribers to that channel will receive updated data
-      errorElement.textContent = ''
+    // Get the available channels and fill the select menu with the channel names.
+    await createChannelsMenu();
 
-      try {
-        glue.channels.publish({ symbol }).then(() => {
-          console.log(
-            'Successfully pushed data to channel with name: ',
-            glue.channels.current()
-          )
-        })
-      } catch (error) {
-        // try catch used since there was a bug as of time of writing, should use catch of the promise instead
-        errorElement.textContent = error
-      }
-    }
+    trackCurrentChannel();
 
-    symbolListElement.appendChild(symbolElement)
-  })
+    handleButtonClicks();
+});
 
-  // the publisher app becomes a subscriber in it's turn by showing info coming from subscriber app
-  glue.channels.subscribe(data => {
-    if (data.ric) {
-      infoFromSubscriberElement.textContent = data.ric
-    }
-  })
+async function initializeGlue42() {
+    // Initialize the Glue42 library and enable the Channels API
+    // and the Channel Selector UI.
+    window.glue = await Glue({ channels: true });
+};
 
-  glue.channels.changed(newChannel => {
-    if (!newChannel) {
-      // handle the case where your app is not joined to any channel
-      // (e.g., the user has deselected the current channel)
-      console.log('INFO: your app is not joined to any channel')
+async function createChannelsMenu() {
+    const allChannels = await glue.channels.all();
+    const optionItem = document.createElement("option");
+
+    allChannels.sort().forEach(channelName => {
+        const currentOption = optionItem.cloneNode();
+        
+        currentOption.innerText = channelName;
+        channelListItem.appendChild(currentOption);
+    });
+};
+
+function trackCurrentChannel() {
+    glue.channels.onChanged(async newChannelName => {
+        if (newChannelName) {
+            // Handle switching to another channel.
+            const newChannelContext = await glue.channels.get(newChannelName);
+
+            channelNameItem.innerText = newChannelName;
+            channelColorItem.style.backgroundColor = newChannelContext.meta.color;
+        } else {
+            // Handle the case where the app is not joined to any channel 
+            // (e.g., the user has deselected the current channel).
+            channelNameItem.innerText = "No Channel";
+            channelColorItem.style.backgroundColor = "";
+        }
+    });
+};
+
+function handleButtonClicks() {
+    publishToCurrentBtn.addEventListener("click", publishToCurrent);
+    publishToSelectedBtn.addEventListener("click", publishToSelected);
+};
+
+async function publishToCurrent() {
+    const currentChannelName = await glue.channels.my();
+
+    if (currentChannelName) {
+        selectChannelAlert.style.display = "none";
+
+        const input = currentChannelInput.value;
+        const dataToPublish = { input };
+
+        glue.channels.publish(dataToPublish);
+        currentChannelInput.value = "";
     } else {
-      // handle the case where you have switched to another channel
-      console.log(
-        'INFO: switched to channel with name: ',
-        glue.channels.current()
-      )
+        selectChannelAlert.style.display = "block";
     }
-  })
-})
+};
+
+function publishToSelected() {
+    const selectedChannelName = channelListItem[channelListItem.selectedIndex].text;
+    const input = selectedChannelInput.value;
+    const dataToPublish = { input };
+
+    glue.channels.publish(dataToPublish, selectedChannelName);
+    selectedChannelInput.value = "";
+};
