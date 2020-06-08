95bab5bb15dcb2bd266073e8b4457aafe0df9577
Hristo Ivanov <47400757+arjunah@users.noreply.github.com>
2020-06-08 19:16:44 +0300

    Pub/Sub examples reworked

--- pub-sub/publisher/src/index.js
+++ pub-sub/publisher/src/index.js
@@ -1,16 +1,41 @@
-Glue({ bus: true }).then((glue) => {
-    window.glue = glue;
+// Topic on which the messages will be published.
+const topic = "Time";
+let startPublishingBtn;
 
+/** SET UP THE APPLICATION **/
+window.addEventListener("DOMContentLoaded", initializeApp);
+
+async function initializeApp() {
+    startPublishingBtn = document.getElementById("button");
+
+    // Initialize the Glue42 library.
+    await initializeGlue42()
+    .catch(error => { 
+        console.error(error); 
+        return 
+    });
+
+    startPublishingBtn.addEventListener("click", startPublishing);
+};
+
+/** INITIALIZE GLUE42 **/
+async function initializeGlue42() {
+    // Initializing the Glue42 library with `bus: true`
+    // in order to be able to use the Pub/Sub API.
+    window.glue = await Glue({ bus: true });
+};
+
+/** PUBLISH MESSAGES AT AN INTERVAL **/
+function startPublishing() {
     setInterval(() => {
-        const time = new Date();
-        const hours = time.getHours();
-        const minutes = time.getMinutes();
-        const seconds = time.getSeconds();
-    
-        glue.bus.publish('time', {
-            hours,
-            minutes,
-            seconds
-        })
+        const currentDate = new Date();
+        const data = {
+            hours: currentDate.getHours(),
+            minutes: currentDate.getMinutes(),
+            seconds: currentDate.getSeconds()
+        };
+
+        // Publish a message on a specific topic.
+        glue.bus.publish(topic, data);
     }, 1000);
-});
+};
--- pub-sub/subscriber/src/index.js
+++ pub-sub/subscriber/src/index.js
@@ -1,7 +1,47 @@
-Glue({ bus: true }).then((glue) => {
-    window.glue = glue;
+// Topic for messages on which to subscribe.
+const topic = "Time";
 
-    glue.bus.subscribe('time', (data) => {
-        document.getElementById('clock').innerHTML = `${data.hours}:${data.minutes}:${data.seconds}`
+let hoursItem;
+let minutesItem;
+let secondsItem;
+
+/** SET UP THE APPLICATION **/
+window.addEventListener("DOMContentLoaded", initializeApp);
+
+async function initializeApp() {
+    hoursItem = document.getElementById("hours");
+    minutesItem = document.getElementById("minutes");
+    secondsItem = document.getElementById("seconds");
+    
+    // Initialize the Glue42 library.
+    await initializeGlue42()
+    .catch(error => { 
+        console.error(error); 
+        return 
     });
-});
+
+    subscribeForMessages();
+};
+
+/** INITIALIZE GLUE42 **/
+async function initializeGlue42() {
+    // Initializing the Glue42 library with `bus: true`
+    // in order to be able to use the Pub/Sub API.
+    window.glue = await Glue({ bus: true });
+};
+
+/** SUBSCRIBE FOR MESSAGES ON A SPECIFIC TOPIC **/
+function subscribeForMessages() {
+    glue.bus.subscribe(topic, handleData);
+};
+
+// Handle the data recieved from the messages.
+function handleData(data) {
+    const hours = data.hours;
+    const minutes = data.minutes;
+    const seconds = data.seconds;
+
+    hoursItem.innerText = hours;
+    minutesItem.innerText = minutes;
+    secondsItem.innerText = seconds;
+};
